f0ef145df69a5c70930f2f95a85987d3e408b024
Ilya Pyatizbyantsev <ilya_2.0@mail.ru>
2021-10-15 02:56:36 +0300

    Deleting fields in class Main, cosmetic changes

--- src/main/homeTasks/lab1/Main.java
+++ src/main/homeTasks/lab1/Main.java
@@ -4,18 +4,14 @@ import main.homeTasks.lab1.strategies.*;
 
 class Main {
 
-    private static StrategyFly balloon = new StrategyFly();
-    private static StrategyRideAHorse horse = new StrategyRideAHorse();
-    private static StrategyWalk legs = new StrategyWalk();
-
     public static void main(String[] args) {
         Hero hero = new Hero();
         hero.move();
 
-        hero.changeMovingStrategy(horse);
+        hero.changeMovingStrategy(new StrategyRideAHorse());
         hero.move();
 
-        hero.changeMovingStrategy(balloon);
+        hero.changeMovingStrategy(new StrategyFly());
         hero.move();
 
     }
