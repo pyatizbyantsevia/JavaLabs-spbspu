b37775621ab965e4788fa524d3a9d75813ff9c88
Ilya Pyatizbyantsev <ilya_2.0@mail.ru>
2021-10-15 18:54:35 +0300

    Deleting wrong constructor in Hero

--- src/main/homeTasks/lab1/Hero.java
+++ src/main/homeTasks/lab1/Hero.java
@@ -4,13 +4,9 @@ import main.homeTasks.lab1.strategies.*;
 
 public class Hero {
 
-    private MoveStrategy moveStrategy;
+    private MoveStrategy moveStrategy = new StrategyWalk();
     private int currentLocation;
 
-    Hero() {
-        moveStrategy = new StrategyWalk();
-        System.out.println("Hero constructed \n" + moveStrategy.heroCondition() + '\n');
-    }
     void changeMovingStrategy(MoveStrategy strategy) {
         moveStrategy = strategy;
         System.out.println(strategy.heroCondition() + '\n');
