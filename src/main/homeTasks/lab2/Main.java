b3346d2a8c23ecd0bf45319783e3b8be760a9757
Ilya Pyatizbyantsev <ilya_2.0@mail.ru>
2021-10-19 23:21:32 +0300

    Adding temp code

--- src/main/homeTasks/lab2/Main.java
+++ src/main/homeTasks/lab2/Main.java
@@ -1,8 +1,34 @@
 package main.homeTasks.lab2;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
-        
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        FileWriter writer = new FileWriter("/home/ilyait/JavaProjects/Global/JavaLabs-spbspu/src/main/homeTasks/lab2/output.txt", false);
+
+        List<String> list = readUsingFiles("/home/ilyait/JavaProjects/Global/JavaLabs-spbspu/src/main/homeTasks/lab2/temp.txt");
+        Integer N = Integer.valueOf(list.get(0));
+
+        int[][] matrix = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = (int) (Math.random() * N);
+                writer.write(String.valueOf(matrix[i][j]) + ' ');
+            }
+            writer.write('\n');
+        }
+    }
+    private static List<String> readUsingFiles(String fileName) throws IOException {
+        Path path = Paths.get(fileName);
+        byte[] bytes = Files.readAllBytes(path);
+        List<String> allLines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        return allLines;
     }
 }
