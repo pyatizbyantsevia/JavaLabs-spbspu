6a466a15a54c8dced0166dc6e4de75553fb17d5d
Ilya Pyatizbyantsev <ilya_2.0@mail.ru>
2021-10-21 17:44:35 +0300

    Adding divideMatrix method in Matrix class

--- src/main/homeTasks/lab2/Matrix.java
+++ src/main/homeTasks/lab2/Matrix.java
@@ -3,19 +3,20 @@ package main.homeTasks.lab2;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Matrix {
 
-    private double[][] matrix = {{1,4,3,4}, {6,2,3,2}, {8,2,3,8}, {1,2,3,2}};
+    private double[][] matrix; //= {{1,4,3,4}, {6,2,3,2}, {8,2,3,8}, {1,2,3,2}};
 
     public Matrix(int size) {
-//        matrix = new double[size][size];
-//        int[][] matrix = {{1,4,3,4}, {6,2,3,2}, {8,2,3,8}, {1,2,3,2}};
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 0; j < N; j++) {
-//                matrix[i][j] = (int) (Math.random() * N);
-//            }
-//        }
+        Random rnd = new Random(System.currentTimeMillis());
+        matrix = new double[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = ((-size) + rnd.nextInt(size - (-size) + 1));
+            }
+        }
     }
 
     public void printMatrix(String fileName) throws IOException {
@@ -45,6 +46,25 @@ public class Matrix {
             for (int j = 0; j < N; j++) {
                 temp[i][j] = (double) fullMatrix.get(0);
                 fullMatrix.remove(0);
+            }
+        }
+        matrix = temp;
+        this.divideMatrix();
+    }
+
+    private void divideMatrix() {
+        int N = matrix.length;
+        double[][] temp = new double[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (j == 0) {
+                    temp[i][j] = matrix[i][j] / matrix[i][j+1];
+                } else if(j == (N - 1)) {
+                    temp[i][j] = matrix[i][j] / matrix[i][j-1];
+                } else {
+                    temp[i][j] = matrix[i][j] / (matrix[i][j-1]+ matrix[i][j+1]);
+                }
             }
         }
         matrix = temp;
