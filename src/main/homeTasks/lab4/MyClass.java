fbcb9f32d3db606272c1f39c38df8cf1e01aa5bd
Илья <ilya_2.0@mail.ru>
2021-12-09 20:14:20 +0300

    Adding Annotation and MyClass

---
+++ src/main/homeTasks/lab4/MyClass.java
@@ -0,0 +1,11 @@
+package main.homeTasks.lab4;
+
+public class MyClass {
+    @MyAnnotation(1)
+    private void firstMethod() {
+    }
+    private void secondMethod() {
+    }
+    private void thirdMethod() {
+    }
+}
