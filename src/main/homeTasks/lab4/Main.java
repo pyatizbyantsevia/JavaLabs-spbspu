088e7ceb2e7a0d9c8dce6c10232f46eb5921ccec
Илья <ilya_2.0@mail.ru>
2021-12-10 00:04:58 +0300

    Adding callAnnotatedMethods() - a method using reflection to call annotated methods

--- src/main/homeTasks/lab4/Main.java
+++ src/main/homeTasks/lab4/Main.java
@@ -1,7 +1,28 @@
 package main.homeTasks.lab4;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
     public static void main(String[] args) {
 
+        MyClass test = new MyClass();
+        try {
+            callAnnotatedMethods(test);
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void callAnnotatedMethods(Object object) throws InvocationTargetException, IllegalAccessException {
+        Class c = object.getClass();
+        for (Method method:c.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(MyAnnotation.class)) {
+                for (int i = 0; i < method.getAnnotation(MyAnnotation.class).value(); i++) {
+                    method.setAccessible(true);
+                    method.invoke(object);
+                }
+            }
+        }
     }
 }
--- src/main/homeTasks/lab4/MyClass.java
+++ src/main/homeTasks/lab4/MyClass.java
@@ -3,8 +3,12 @@ package main.homeTasks.lab4;
 public class MyClass {
     @MyAnnotation(1)
     private void firstMethod() {
+        System.out.println("Первый вызван");
     }
+
+    @MyAnnotation(2)
     private void secondMethod() {
+        System.out.println("Второй вызван");
     }
     private void thirdMethod() {
     }
