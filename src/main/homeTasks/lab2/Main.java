6c0a333b68598943d7611aca4ad976011667c621
Илья <ilya_2.0@mail.ru>
2021-12-20 17:34:37 +0300

    Refactor for absolute path for input file in lab2

--- src/main/homeTasks/lab2/Main.java
+++ src/main/homeTasks/lab2/Main.java
@@ -4,41 +4,45 @@ import main.homeTasks.lab2.Exceptions.DivideNullException;
 import main.homeTasks.lab2.Exceptions.MatrixSizeOverflow;
 import main.homeTasks.lab2.Exceptions.UndefinedFileException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-    final static String inputFilePath = "C:\\Users\\ilya_\\Documents\\Projects\\Java\\JavaLabs-spbspu\\src\\main\\homeTasks\\lab2\\temp.txt";
+    final static String sourceName = "main/homeTasks/lab2/resources/temp.txt";
 
     public static void main(String[] args) {
+        System.out.println(sourceName);
+        File source = new File(sourceName);
         try {
-            int N = readUsingFile();
+            int N = getN(source);
             Matrix matrix = new Matrix(N);
             matrix.printMatrix("output.txt");
             for (int i = 0; i < 3; i++) {
                 matrix.divideAndRotateMatrix();
                 matrix.printMatrix("output.txt");
             }
-        } catch (IOException | DivideNullException | MatrixSizeOverflow | UndefinedFileException err) {
+        } catch (IOException | MatrixSizeOverflow | UndefinedFileException | DivideNullException err) {
             System.err.println(err.getMessage());
         }
     }
 
-    private static int readUsingFile() throws UndefinedFileException {
-        Path path = Paths.get(inputFilePath);
-        List<String> allLines;
-        int temp;
-        try {
-            allLines = Files.readAllLines(path, StandardCharsets.UTF_8);
-            temp = Integer.parseInt(allLines.get(0));
-        } catch (IOException | NumberFormatException | IndexOutOfBoundsException e) {
-            throw new UndefinedFileException();
+    public static int getN(File file) throws UndefinedFileException, FileNotFoundException {
+        try (Scanner in = new Scanner(file)) {
+            if (in.hasNextInt()) {
+                int n = in.nextInt();
+                return n;
+            } else {
+                throw new UndefinedFileException();
+            }
         }
-        return temp;
     }
+
 }
