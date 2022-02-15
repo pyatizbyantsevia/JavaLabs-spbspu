73a12f0490448b640ebb68ce63c2a1d13cf5d501
Илья <ilya_2.0@mail.ru>
2022-02-15 20:43:46 +0300

    Backup changes

--- src/main/homeTasks/lab6/AbstractProgram.java
+++ src/main/homeTasks/lab6/AbstractProgram.java
@@ -1,5 +1,7 @@
 package main.homeTasks.lab6;
 
+import java.util.Random;
+
 class AbstractProgram implements Runnable {
 
     public static Condition abstractProgCondition = Condition.UNKNOWN;
@@ -14,6 +16,15 @@ class AbstractProgram implements Runnable {
     @Override
     public void run() {
         while (true) {
+            Thread daemon = new Thread() {
+              @Override
+              public void run()
+              {
+                  Random random = new Random();
+                  abstractProgCondition.
+              }
+            };
+            daemon.setDaemon(true);
             Utils.sleep(3000);
             System.out.println("I am AbstractProgram and I'm working");
         }
